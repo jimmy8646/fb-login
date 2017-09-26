4988494d79de7149be182adbbf28861e8c35b1a6
jimmy8646 <yujiechueh@gmail.com>
2017-09-27 01:18:03 +0800

    echo user name

--- index.php
+++ index.php
@@ -14,8 +14,23 @@ $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('http://'.$_SERVER['HTTP_HOST'].'/fb-callback.php', $permissions);
 
-if($_SESSION['fb_access_token']){
-    echo $_SESSION['fb_access_token'];
+if(isset($_SESSION['fb_access_token'])){
+    $token=$_SESSION['fb_access_token'];
+
+    try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get('/me?fields=id,name', $token);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$user = $response->getGraphUser();
+
+echo 'Name: ' . $user->getName();
 }else {
     echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
 }
